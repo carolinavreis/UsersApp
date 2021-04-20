793cbce434b80f94175198a89d020055e3f7903c
carolinavreis <me@carolinareis.net>
2021-04-20 15:59:56 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return(
+        <>
+        <Head>
+        <title> Users List | Home </title>
+        <meta name="keywords" content="users" />
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>lorem</p>
+            <p>lorem</p>
+        </div>
+        </>
+    );
+} 
+
+export default About;
